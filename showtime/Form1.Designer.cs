8a2ccb67ea5bf536d6f4f68988367e2f8b58a4d6
5b2g0001 <5b2g0001@stust.edu.tw>
2026-03-30 15:04:24 +0800

    加入專案檔案。

---
+++ showtime/Form1.Designer.cs
@@ -0,0 +1,192 @@
+﻿namespace showtime
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            textBox4 = new TextBox();
+            textBox5 = new TextBox();
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            button1 = new Button();
+            button2 = new Button();
+            SuspendLayout();
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(217, 71);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(100, 23);
+            textBox1.TabIndex = 0;
+            textBox1.TextChanged += textBox1_TextChanged;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(217, 100);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(308, 23);
+            textBox2.TabIndex = 1;
+            textBox2.TextChanged += textBox2_TextChanged;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(217, 180);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(100, 23);
+            textBox3.TabIndex = 2;
+            textBox3.TextChanged += textBox3_TextChanged;
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(217, 209);
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(100, 23);
+            textBox4.TabIndex = 3;
+            textBox4.TextChanged += textBox4_TextChanged;
+            // 
+            // textBox5
+            // 
+            textBox5.Location = new Point(217, 238);
+            textBox5.Name = "textBox5";
+            textBox5.Size = new Size(308, 23);
+            textBox5.TabIndex = 4;
+            textBox5.TextChanged += textBox5_TextChanged;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(148, 79);
+            label1.Name = "label1";
+            label1.Size = new Size(55, 15);
+            label1.TabIndex = 5;
+            label1.Text = "接聽port";
+            label1.Click += 接聽port_Click;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(148, 108);
+            label2.Name = "label2";
+            label2.Size = new Size(55, 15);
+            label2.TabIndex = 6;
+            label2.Text = "接收文字";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Enabled = false;
+            label3.Location = new Point(148, 188);
+            label3.Name = "label3";
+            label3.Size = new Size(42, 15);
+            label3.TabIndex = 7;
+            label3.Text = "目標ip";
+            label3.Click += label3_Click;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(148, 217);
+            label4.Name = "label4";
+            label4.Size = new Size(55, 15);
+            label4.TabIndex = 8;
+            label4.Text = "目標port";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(148, 246);
+            label5.Name = "label5";
+            label5.Size = new Size(55, 15);
+            label5.TabIndex = 9;
+            label5.Text = "傳送文字";
+            // 
+            // button1
+            // 
+            button1.Location = new Point(540, 100);
+            button1.Name = "button1";
+            button1.Size = new Size(75, 23);
+            button1.TabIndex = 10;
+            button1.Text = "啟動接收";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // button2
+            // 
+            button2.Location = new Point(540, 238);
+            button2.Name = "button2";
+            button2.Size = new Size(75, 23);
+            button2.TabIndex = 11;
+            button2.Text = "傳送";
+            button2.UseVisualStyleBackColor = true;
+            button2.Click += button2_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(button2);
+            Controls.Add(button1);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(textBox5);
+            Controls.Add(textBox4);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Name = "Form1";
+            Text = "UOP即時通";
+            Load += Form1_Load;
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private TextBox textBox4;
+        private TextBox textBox5;
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Button button1;
+        private Button button2;
+    }
+}
---
+++ showtime/Form1.cs
@@ -0,0 +1,179 @@
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Text;
+
+namespace showtime
+{
+    public partial class Form1 : Form
+    {
+        UdpClient U;
+        Thread Th;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        //監聽副程序
+        private void button1_Click(object sender, EventArgs e)
+        {
+            if (U != null)
+            {
+                MessageBox.Show("監聽已啟動！");
+                return;
+            }
+
+            Th = new Thread(Listen);
+            Th.IsBackground = true;
+            Th.Start();
+            button1.Enabled = false;
+        }
+
+        private volatile bool listening = false;
+
+        private void Listen()
+        {
+            if (!int.TryParse(textBox1.Text, out int Port))
+            {
+                MessageBox.Show("請輸入正確的監聽 Port");
+                return;
+            }
+
+            try
+            {
+                U = new UdpClient(Port);
+            }
+            catch (SocketException)
+            {
+                MessageBox.Show("Port 已被占用，請換一個");
+                return;
+            }
+
+            IPEndPoint EP = new IPEndPoint(IPAddress.Any, Port);
+
+            listening = true;
+
+            while (listening)
+            {
+                try
+                {
+                    byte[] B = U.Receive(ref EP);
+                    string msg = Encoding.UTF8.GetString(B);
+
+                    this.Invoke((MethodInvoker)(() =>
+                    {
+                        textBox2.AppendText(msg + Environment.NewLine);
+                    }));
+                }
+                catch (ObjectDisposedException)
+                {
+                    // UdpClient 被關閉時，Receive 會拋此例外，跳出迴圈
+                    break;
+                }
+                catch (SocketException)
+                {
+                    // 監聽過程中 Socket 例外，可視需求處理
+                    break;
+                }
+            }
+        }
+
+        protected override void OnFormClosing(FormClosingEventArgs e)
+        {
+            try
+            {
+                listening = false;
+                U?.Close();
+                // 不用 Thread.Abort，等執行緒自然結束
+                Th?.Join(1000); // 等待執行緒結束
+            }
+            catch { }
+
+            base.OnFormClosing(e);
+        }
+
+        //發送 UDP 訊息
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string IP = textBox3.Text;
+            //設定發送目標 IP
+            int Port = int.Parse(textBox4.Text);
+            //設定發送目標 Port
+            byte[] B = Encoding.UTF8.GetBytes(textBox5.Text);
+            //字串翻譯成位元組陣列
+            UdpClient S = new UdpClient();
+            // 建立 UDP 通訊器
+            S.Send(B, B.Length, IP, Port);
+            //發送資料到指定位置
+            S.Close();
+            //關閉通訊器
+
+        }
+
+        //找出本機 IP
+        private string MyIP()
+        {
+            string hn = Dns.GetHostName();
+            //取得本機電腦名稱
+            IPAddress[] ip = Dns.GetHostEntry(hn).AddressList;
+            //取得本機IP 陣列
+            {
+                foreach (IPAddress it in ip)
+                    // 列舉各個IP
+                    if (it.AddressFamily == AddressFamily.InterNetwork)
+                    //如果是IPv4 格式
+                    {
+                        return it.ToString();
+                    }
+            }
+            //傳回此IP 字串
+            return "";
+        }
+        //找不到合格 IP,回傳空字串
+        //表單載入
+        private void Forml_Load(object sender, EventArgs e)
+        {
+            this.Text += " " + MyIP();
+        }
+        //顯示本機IP於標題列
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void 接聽port_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
